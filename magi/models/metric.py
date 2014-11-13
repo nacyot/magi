b4e3217f63dc2e140410b7827832d824639bbd0e
Eunchong Yu <kroisse@gmail.com>
2014-11-13 16:45:42 +0900

    Improve models

--- magi/models/metric.py
+++ magi/models/metric.py
@@ -2,33 +2,50 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
+import uuid
+
 from sqlalchemy import types
 from sqlalchemy.dialects import postgresql
-from sqlalchemy.schema import Column
+from sqlalchemy.schema import Column, ForeignKey, ForeignKeyConstraint
 
 from ..orm import Base
 
 
-class Host(Base):
+CASCADE = 'CASCADE'
+
+
+class Agent(Base):
+    """Represent the individual Magi agent"""
     id = Column(postgresql.UUID, primary_key=True, default=uuid.uuid4)
-    hostname = Column(types.String(48), nullable=True)
 
 
 class Metric(Base):
-    """"""
-    host_id = Column(ForeignKey(Host.id), primary_key=True)
+    """Represent the metrics sent from the Magi agent"""
+    agent_id = Column(ForeignKey(Agent.id,
+                                 onupdate=CASCADE,
+                                 ondelete=CASCADE),
+                      primary_key=True)
     id = Column(types.String(80), primary_key=True)
+    target_id = Column(types.Unicode(80), nullable=False)
     plugin_name = Column(types.String(80), nullable=True)
+    metric_name = Column(types.String(40), nullable=True)
     updated_at = Column(types.DateTime, nullable=False)
     data = Column(postgresql.JSON, nullable=False)
 
 
 class Log(Base):
-    """"""
-    host_id = Column(ForeignKey(Metric.host_id), primary_key=True)
-    metric_id = Column(ForeignKey(Metric.id,
-                                  onupdate='CASCADE',
-                                  ondelete='CASCADE'),
-                       primary_key=True)
+    """Raw data of each metrics"""
+    agent_id = Column(primary_key=True)
+    metric_id = Column(primary_key=True)
     timestamp = Column(types.DateTime, primary_key=True)
+    key = Column(types.Unicode(256), nullable=True)
     value = Column(postgresql.JSON, nullable=False)
+
+    __table_args__ = (
+        ForeignKeyConstraint(
+            [agent_id, metric_id],
+            [Metric.agent_id, Metric.id],
+            onupdate=CASCADE, ondelete=CASCADE,
+        ),
+        {}
+    )
