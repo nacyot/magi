400f20eb8397f616e0b32125aa21b952cc671180
Jihyeok Seo <me@limeburst.net>
2014-11-13 13:48:44 +0900

    Magi web

---
+++ magi/web/__init__.py
@@ -0,0 +1,18 @@
+""":mod:`magi.web` --- Magi web
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """Home."""
+    return render_template('home.html')
+
+
+if __name__=="__main__":
+    app.run(debug=True)
