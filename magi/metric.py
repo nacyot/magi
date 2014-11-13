10910643c71693e34f3adc1a7d67b565a99edee0
Eunchong Yu <kroisse@gmail.com>
2014-11-13 17:04:52 +0900

    More improvements of models

--- magi/metric.py
+++ magi/metric.py
@@ -16,29 +16,36 @@ CASCADE = 'CASCADE'
 
 class Agent(Base):
     """Represent the individual Magi agent"""
-    id = Column(UUID, primary_key=True, default=uuid.uuid4)
-
     __tablename__ = 'agents'
+
+    #: Unique identifier of the agent
+    #:
+    #: Each agent will choose appropriate id, and send it to the headquarter
+    #: to identify themselves.
+    id = Column(UUID, primary_key=True, default=uuid.uuid4)
+    name = Column(Unicode(80), nullable=False)
 
 
 class Metric(Base):
     """Represent the metrics sent from the Magi agent"""
+    __tablename__ = 'metrics'
+
     agent_id = Column(ForeignKey(Agent.id,
                                  onupdate=CASCADE,
                                  ondelete=CASCADE),
                       primary_key=True)
+    #: Unique identifier of each metric.
     id = Column(String(80), primary_key=True)
     target_id = Column(Unicode(80), nullable=False)
-    plugin_name = Column(String(80), nullable=True)
-    metric_name = Column(String(40), nullable=True)
+    metric_name = Column(String(40), nullable=False)
     updated_at = Column(DateTime, nullable=False)
     data = Column(JSON, nullable=False)
-
-    __tablename__ = 'metrics'
 
 
 class Log(Base):
     """Raw data of each metrics"""
+    __tablename__ = 'logs'
+
     agent_id = Column(primary_key=True)
     metric_id = Column(primary_key=True)
     timestamp = Column(DateTime, primary_key=True)
@@ -53,5 +60,3 @@ class Log(Base):
         ),
         {}
     )
-
-    __tablename__ = 'logs'
