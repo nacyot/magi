20994e691a219b87cd2627755fa3ebf205bdcec8
Eunchong Yu <kroisse@gmail.com>
2014-11-13 17:58:31 +0900

    We're moving to Py3k!

--- setup.py
+++ setup.py
@@ -38,8 +38,9 @@ setup(
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
         'Topic :: System :: Networking :: Monitoring'
